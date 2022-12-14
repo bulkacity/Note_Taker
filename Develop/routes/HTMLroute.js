84f0827a7036ffe295700791ded7b1faa70ba9f5
bulkacity <112503952+bulkacity@users.noreply.github.com>
2022-12-14 15:15:27 -0500

    created an index.js file

--- Develop/routes/HTMLroute.js
+++ Develop/routes/HTMLroute.js
@@ -1,8 +1,5 @@
 const htmlRouter  = require('express').htmlRouter ();
-const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 const path = require('path');
-
 
 htmlRouter .get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const { loggerAPP } = require('./middleware/loggerApp');
 const api = require('./Develop/routes/index');
 
@@ -16,16 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 app.use(express.static('public'));
-
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
